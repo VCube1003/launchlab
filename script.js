d33b2c7b6b1d19a9f60ef58c5a03652ae32595a9
VCube1003 <virajdatla0204@gmail.com>
2026-03-20 20:50:17 +0530

    added menu buttons, level selector screen buttons

---
+++ script.js
@@ -0,0 +1,19 @@
+levels.classList.add("hidden");
+levelone.classList.add("hidden");
+function startGame() {
+    menu.classList.add("hidden");
+    levels.classList.remove("hidden"); 
+}
+function saveGame() {
+    alert("Save Game");
+}
+function loadGame() {
+    alert("Load Game")
+}
+function howToPlay(){
+    alert("How to Play")
+}
+function level1() {
+    levels.classList.add("hidden");
+    levelone.classList.remove("hidden"); 
+}
